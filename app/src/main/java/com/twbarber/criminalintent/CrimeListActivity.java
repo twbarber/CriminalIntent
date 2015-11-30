573c0ea220029e27c8aa8fe9aab5195241127338
Tyler Barber <tylerwbarber@gmail.com>
2015-11-30 17:00:03 -0600

    Working on Chapter 9, creating and populating RecyclerView

---
+++ app/src/main/java/com/twbarber/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.twbarber.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tyler on 11/30/15.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/twbarber/criminalintent/CrimeListFragment.java
@@ -0,0 +1,28 @@
+package com.twbarber.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tyler on 11/30/15.
+ */
+public class CrimeListFragment extends Fragment {
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view
+                .findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+}
